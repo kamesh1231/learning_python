a8f4909e53496adfb322e2e958acf244b80efec4
kamesh <kameswar.pindiprole@gmail.com>
2022-09-29 20:47:45 +1000

    2nd version of add program

--- add_procedural.py
+++ add_procedural.py
@@ -2,7 +2,11 @@
 x = 20
 # Get the 2nd number
 y = 30
+a = 18
+b = 22
 # add both the numbers and get results
-sum = x + y
+sum1 = x + y
+sum2 = a + b
 # print sum
-print(sum)
+print(sum1)
+print(sum2)
