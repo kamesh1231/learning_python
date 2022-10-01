07b314411eae1ab1cf9b2aa00578cdaca19dea58
kamesh <kameswar.pindiprole@gmail.com>
2022-10-01 22:16:55 +1000

    subtract commit

--- subtract_procedural.py
+++ subtract_procedural.py
@@ -2,7 +2,7 @@
 x = 20
 # get the 2nd number
 y = 10
-# subtract both the numbers and get the results
+# subtract both the  numbers and get the results
 Minus = x - y
 # print the result
 print(Minus)
