619c8750d98b2556936c5606bbea091a509532ca
kamesh <kameswar.pindiprole@gmail.com>
2022-10-01 22:21:09 +1000

    division commit

--- division_procedural.py
+++ division_procedural.py
@@ -2,7 +2,7 @@
 x = 25
 # get the 2nd number
 y = 5
-# divide both the numbers and get the results
+# divide  both the numbers and get the results
 division = x / y
 # print the result
 print(division)
