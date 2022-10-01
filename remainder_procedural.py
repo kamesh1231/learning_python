c4959f856fd7645ebcbc6c4e942f22c0391edc34
kamesh <kameswar.pindiprole@gmail.com>
2022-10-01 22:18:14 +1000

    remainder commit

--- remainder_procedural.py
+++ remainder_procedural.py
@@ -2,7 +2,7 @@
 x = 27
 # get the 2nd number
 y = 5
-# do remainder procedure and get the results
+# do  remainder procedure and get the results
 remainder = x % y
 # print the result
 print(remainder)
