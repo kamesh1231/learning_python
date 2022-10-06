b8e84308c7584b65f97140f8d492e87de0f20da3
kamesh <kameswar.pindiprole@gmail.com>
2022-10-06 22:32:11 +1100

    add using define function

---
+++ addition_function.py
@@ -0,0 +1,13 @@
+def sum(x, y):
+    sum1 = int(x) + int(y)
+    print(sum1)
+
+
+print("enter first input")
+a = input()
+print("first value is " + a)
+print("enter second input")
+b = input()
+print("second value is " + b)
+
+sum(a, b)
