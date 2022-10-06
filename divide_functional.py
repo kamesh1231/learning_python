55c1532967ac235473718a1ff0dc631209e65630
kamesh <kameswar.pindiprole@gmail.com>
2022-10-06 22:37:57 +1100

    divide using define function

---
+++ divide_functional.py
@@ -0,0 +1,13 @@
+def div(x, y):
+    div1 = int(x) / int(y)
+    print(div1)
+
+
+print("enter first input")
+a = input()
+print("first value is " + a)
+print("enter second input")
+b = input()
+print("second value is " + b)
+
+div(a, b)
