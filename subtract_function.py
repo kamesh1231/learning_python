25eaaff3b1ded31ac40dccde4097abb74462cc27
kamesh <kameswar.pindiprole@gmail.com>
2022-10-06 22:34:09 +1100

    subtract using define function

---
+++ subtract_function.py
@@ -0,0 +1,13 @@
+def subt(x,y):
+    subt1= int(x)-int(y)
+    print(subt1)
+
+
+print("enter first input")
+a = input()
+print("first value is " + a)
+print("enter second input")
+b = input()
+print("second value is " + b)
+
+subt(a, b)
