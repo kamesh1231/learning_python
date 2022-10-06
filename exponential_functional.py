de96ea77f3f193748980300877e6c0953914ae2f
kamesh <kameswar.pindiprole@gmail.com>
2022-10-06 22:40:12 +1100

    exponential using define function

---
+++ exponential_functional.py
@@ -0,0 +1,13 @@
+def exp(x, y):
+    exp1 = int(x) ** int(y)
+    print(exp1)
+
+
+print("enter first input")
+a = input()
+print("first value is " + a)
+print("enter second input")
+b = input()
+print("second value is " + b)
+
+exp(a, b)
