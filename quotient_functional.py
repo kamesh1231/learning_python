b9a02d3b21324c7ab88e1cd938a7de39cadad8bd
kamesh <kameswar.pindiprole@gmail.com>
2022-10-06 22:42:44 +1100

    exponential using define function

---
+++ quotient_functional.py
@@ -0,0 +1,13 @@
+def quot(x, y):
+    quot1 = int(x) // int(y)
+    print(quot1)
+
+
+print("enter first input")
+a = input()
+print("first value is " + a)
+print("enter second input")
+b = input()
+print("second value is " + b)
+
+quot(a, b)
