0f7dc69fe0f7fac864941bb333819eb3d9bd5070
kamesh <kameswar.pindiprole@gmail.com>
2022-10-12 21:04:29 +1100

    changes

--- remainder_procedural.py
+++ remainder_procedural.py
@@ -1,8 +1,19 @@
-# get the 1st number
-x = 27
-# get the 2nd number
-y = 5
-# do  remainder procedure and get the results
-remainder = x % y
+print("enter first input")
+a = input()
+if int(a) < 0:
+    print("not valid")
+    exit()
+else:
+    print("first value is " + a)
+print("enter second input")
+b = input()
+if int(b) < 0:
+    print("not valid ")
+    exit()
+else:
+    print("second value is " + b)
+
+
+remainder = int(a) % int(b)
 # print the result
 print(remainder)
