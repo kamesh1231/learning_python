99a78cdb1245225ba23d8aa9c5ab128d836b8270
kamesh <kameswar.pindiprole@gmail.com>
2022-10-13 21:41:02 +1100

    change 3

--- calculator.py
+++ calculator.py
@@ -8,12 +8,32 @@ inpt = input("enter choice(1/2): ")
 
 if inpt == '1':
     a = float(input("enter the first number"))
+    if int(a) < 0:
+        print("not valid")
+        exit()
+    else:
+        print("first value is " + str(a))
     b = float(input("enter the second number"))
+    if int(b) < 0:
+        print("not valid ")
+        exit()
+    else:
+        print("second value is " + str(b))
     print(int(a) + int(b))
 
 elif inpt == '2':
     a = float(input("enter the first number"))
+    if int(a) < 0:
+        print("not valid")
+        exit()
+    else:
+        print("first value is " + str(a))
     b = float(input("enter the second number"))
+    if int(b) < 0:
+        print("not valid ")
+        exit()
+    else:
+        print("second value is " + str(b))
     print(int(a) - int(b))
 else:
     print("invalid")
