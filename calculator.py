7c6b4ea503bb7a556e72e426bbc2545cbf4ffd52
kamesh <kameswar.pindiprole@gmail.com>
2022-10-13 21:35:09 +1100

    change 3

--- calculator.py
+++ calculator.py
@@ -1,24 +1,19 @@
-def add(x,y):
-    add1 = int(x) + int(y)
-    print(add1)
-
-def subtract(x,y):
-    subtract1 = int(x) - int(y)
-    print(subtract1)
 
 print("select operation?")
 print("1.Add")
 print("2.Subtract")
 
-while True:
-    inpt = input("enter choice(1/2): ")
-    if inpt in('1', '2'):
-        a = float(input("enter the first number"))
-        b = float(input("enter the second number"))
+inpt = input("enter choice(1/2): ")
 
-        if inpt == '1':
-            print(int(a) + int(b))
-        elif inpt == '2':
-            print(int(a) - int(b))
-        else:
-            print("invalid")
+
+if inpt == '1':
+    a = float(input("enter the first number"))
+    b = float(input("enter the second number"))
+    print(int(a) + int(b))
+
+elif inpt == '2':
+    a = float(input("enter the first number"))
+    b = float(input("enter the second number"))
+    print(int(a) - int(b))
+else:
+    print("invalid")
