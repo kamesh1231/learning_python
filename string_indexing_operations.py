03dcfb90ee34b92af8857eac20f7005980981d94
kamesh <kameswar.pindiprole@gmail.com>
2022-11-02 03:23:34 +1100

    string_indexing-update1

--- string_indexing_operations.py
+++ string_indexing_operations.py
@@ -1,6 +1,7 @@
 print("you have chosen the index function")
 print("index function Searches the string for a specified value and returns the position of where it was found")
 h1 = input("what is the string?")
+print("You entered :" + h1)
 h2 = h1[0]
 h3 = h1[len(h1)-1]
 a = (len(h1)) % 2
@@ -8,11 +9,15 @@ print("The first alphabet of the string is : " + h2)
 print("The last alphabet of the string is : " + h3)
 print("The length of the string is :  " + str(len(h1)))
 
-if a % 2 == 1:
-    h4 = h1[int(len(h1)/2)]
+if a == 1:
+    b = int(len(h1)/2)
+    print("mid index is " +str(b))
+    h4 = h1[b]
     print("The middle character is : " + h4)
 
-elif a % 2 == 0:
-    h5 = h1[int((len(h1)/2) - 1)]
-    h6 = h1[int((len(h1) / 2) + 1)]
-    print("The middle character is : " + h5 + " and " + h6  )
+elif a == 0:
+    b = int(len(h1) / 2)
+    print("mid index is " + str(b))
+    h5 = h1[b - 1]
+    h6 = h1[b]
+    print("The middle character is : " + h5 + " and " + h6)
