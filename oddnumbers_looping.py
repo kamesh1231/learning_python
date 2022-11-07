b1ac7a94262184aa5d235d8177cb511a3304c7f7
kamesh <kameswar.pindiprole@gmail.com>
2022-11-07 18:42:39 +1100

    looping_operations

---
+++ oddnumbers_looping.py
@@ -0,0 +1,20 @@
+a = input("Enter a number:")
+print("you entered <" +a +">")
+b = input("How many odd numbers you wish to print?")
+print("Printing " +b + " odd numbers after " +a)
+if int(a) % 2 == 0:
+    c = int(a) + 1
+    i = 0
+    while int(i) < int(b):
+        print(c,end=" ")
+        i+=1
+        c+=2
+
+elif int(a) % 2 == 1:
+    c = int(a) + 2
+    i = 0
+    while int(i) < int(b):
+        print(c,end=" ")
+        i+=1
+        c+=2
+
